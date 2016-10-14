c43060ab5ef9558467e756a198a31ace2d5b7e65
David Hallum <david@kenzielane.com>
2016-10-14 08:01:28 -0400

    Removing non-existant agent flag [#132157617]

--- ecs_entrypoint.sh
+++ ecs_entrypoint.sh
@@ -4,7 +4,6 @@ IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 | tr -d '\n')
 echo "IP=${IP}"
 
 docker-entrypoint.sh agent          \
-  -agent                            \
   -advertise $IP                    \
   -dc ${DATACENTER}                 \
   -atlas ${ENV_NAME}                \
